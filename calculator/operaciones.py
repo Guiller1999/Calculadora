12d50d15a0a2237cd27be2037f80ad68b15d3a1f
Rivera <Riverareyes_2611@hotmail.com>
2020-05-07 21:28:55 -0500

    Se agregó comentarios

--- calculator/operaciones.py
+++ calculator/operaciones.py
@@ -3,24 +3,38 @@ from calculator import validations
 
 class Operaciones():
 
+    # Nota:
+    # 1) Se usa una variable "num" debido a que así se va acumulando los números que va ingresando el 
+    #    usuario hasta que este pulse un botón de operación, con esto se logra poder validar de forma más
+    #    fácil el número ingresado, evitando que hayan ceros a la izquierda.
+    # 2) Se usa una variable expr_math para almacenar la expresion matemática a calcular, la cual esta
+    #    compuesta por el contenido de la variable num y de la variable operator. La diferencia que tiene
+    #    el contenido de la variable expr_math con respecto a screenText es que en expr_math ciertas
+    #    operaciones se cambian para que se puedan evaluar como por ejemplo para la potencia en vez de
+    #    que sea ^2 como nos muestra la calculadora en expr_math se lo guarda como **2.
+
     def __init__(self):
 
-        self.screenText = StringVar()
-        self.expr_math = ""
-        self.expr_result = StringVar()
-        self.num = ""
-        self.operator = ""
-        self.result = ""
+        self.screenText = StringVar() # Contendrá la expresión matemática que se va a mostrar
+        self.expr_math = "" # Guardará la expresión matemática que se va a calcular
+        self.expr_result = StringVar() # Guardará el resultado a mostrar al usuario
+        self.num = "" # Guardará el número que se está digitando
+        self.operator = "" # Guardará el operador de la operación pulsada por el usuario
+        self.result = "" # Servirá para mantener el resultado en memoria
 
-
+    # Función que es llamada cuando se pulsa un boton de la calculadora
     def push_button(self, button):
         
+        # Se llama a la función correspondiente para validar el inreso de un número
+        # Una vez validado se devuelve el número pulsado, la expresion a mostrar y la
+        # expresión matemática a evaluar
         values = validations.validate_numberEntry(button, self.screenText.get(), self.num, self.expr_math)
 
         self.num = values[0]
         self.screenText.set(values[1])
         self.expr_math = values[2]
 
+    # Función que es llamda cuando se pulsa el signo "=" en la calculadora
     def resultado(self):
         
         try:
@@ -37,6 +51,7 @@ class Operaciones():
 
         self.operator = ""
 
+    # Función que es llamada cuando se pulsa un botón para realizar una operación
     def push_operation(self, button):
 
         self.operator = button
@@ -45,7 +60,8 @@ class Operaciones():
         self.expr_math = values[1]
         self.num = ""
 
-    
+    # Función que es llamada cuando se pulsa el botón "AC" de la calculadora
+    # Se encarga de limpiar la pantalla de la calculadora
     def clear_all(self):
 
         self.screenText.set("")
@@ -53,12 +69,15 @@ class Operaciones():
         self.expr_math = ""
         self.num = ""
     
+    # Función que es llamada cuando se pulsa el botón "(" ó ")"
     def push_parentesis(self, button):
 
         values = validations.validate_parenthesisEntry(button, self.screenText.get(), self.expr_math)
         self.screenText.set(values[0])
         self.expr_math = values[1]
 
+    # Función que es llamada cuando se pulsa el botón "DEL" de la calculadora
+    # Se encarga de eliminar el último carácter que se ingreso
     def delete(self):
 
         texto = self.screenText.get()
@@ -71,6 +90,7 @@ class Operaciones():
         self.expr_math = values[0]
         self.num = values[1]
 
+    # Función que es llamada cuando se pulsa los botones "^2" ó "EXP"
     def potencia(self, button):
 
         values = validations.validate_powEntry(button, self.screenText.get(), self.expr_math)
@@ -78,12 +98,14 @@ class Operaciones():
         self.expr_math = values[1]
         self.num = ""
 
+    # Función que es llamada cuando se pulsa el botón "ANS"
+    # Se encarga de obtener el último resultado que se obtuvo
     def get_resultado(self):
         
         self.screenText.set(self.screenText.get() + str(self.result))
         self.expr_math += str(self.result)
 
-    
+    # Función que es llamada cuando se pulsa el botoón "π"
     def insertPI(self, button):
 
         values = validations.validate_numberPIEntry(button, self.screenText.get(), self.expr_math)
--- calculator/validations.py
+++ calculator/validations.py
@@ -1,34 +1,42 @@
 
+# Función que es llamada cuando se pulsa un número
+# Se encarga de validar que la expresión para poder ingresar con exito un número y no haya prblemas
+# a la hora de evaluar la expresión
 def validate_numberEntry(button, screenText, num, expr_math):
 
     last_num = screenText
     last_num = last_num[len(last_num) - 1 : len(last_num)]
 
+    # Se encarga de validar si el botón pulsado es un número o punto decimal
     if len(num) >= 0 and num != "0" and last_num != ")" and last_num != "π":
         screenText += button
         num += button
         expr_math += button
-    elif len(num) == 1 and last_num == "0" and button.isdigit():
-        text = screenText
+    elif len(num) == 1 and last_num == "0" and button.isdigit(): # Valida que no hayan ceros inecesarios a la
+        text = screenText                                        # izquierda del número ingresado
         text = text[0 : len(text) - 1]
         num = num[0 : len(num) - 1] + button
         expr_math = expr_math[0 : len(expr_math) - 1] + button
         screenText = text + button
-    elif last_num == ")" or last_num == "π" and button.isdigit():
-        screenText += "x" + button
-        expr_math += "*" + button
+    elif last_num == ")" or last_num == "π" and button.isdigit(): # Si el caracter anterior es un ")"
+        screenText += "x" + button                                # o "π", se agregue a la expresion el signo
+        expr_math += "*" + button                                 # de multiplicación
 
     return [num, screenText, expr_math]
 
 
+# Valida el correcto ingreso de los operadores
 def validate_operationEntry(operation, screenText, expr_math):
 
     operators = ["+", "-", "x", "/"]
     last_character = screenText
     last_character = last_character[len(last_character) -1 : len(last_character)]
 
+    # Valida que no se permita ingresar consecutivamente operadores como por ej: ++
     if last_character not in operators:
 
+        # Si el operador a ingresar es la multiplicación se cambia el signo por "*" para que no cause
+        # problemas la expresion matemática a calcular
         if operation == "*":
             screenText += "x"
             expr_math += "*"
@@ -39,6 +47,8 @@ def validate_operationEntry(operation, screenText, expr_math):
     return [screenText, expr_math]
 
 
+# Valida el correcto ingreso de la potencia y se encarga de cambiar el signo de la potencia para 
+# que se calcule correctamente la expresión matemática
 def validate_powEntry(button, screenText, expr_math):
     
     if button == "^2":
@@ -51,6 +61,7 @@ def validate_powEntry(button, screenText, expr_math):
     return [screenText, expr_math]
 
 
+# Valida el correcto ingreso de los parentesis
 def validate_parenthesisEntry(button, screenText, expr_math):
 
     last_character = screenText
@@ -67,6 +78,8 @@ def validate_parenthesisEntry(button, screenText, expr_math):
     return [screenText, expr_math]
 
 
+# Valida que se borre correctamente el último caracter de la expresión matemática
+# para que no haya errores a la hora de calcular la expresión
 def validate_characterDelete(character, expr_math, num):
     
     if character.isdigit():
@@ -83,6 +96,7 @@ def validate_characterDelete(character, expr_math, num):
     return [expr_math, num]
 
 
+# Valida el correcto ingreso del número PI
 def validate_numberPIEntry(button, screenText, expr_math):
 
     last_character = expr_math
